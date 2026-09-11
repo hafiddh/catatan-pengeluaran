120ff1449d029d49c6115e0fbb82c813d19839e4
HafidDH <hafiddwih@gmail.com>
2026-09-11 15:14:01 +0700

    feat: port login page, cookie-based Google sign-in, server-side redirect if authed

---
+++ components/login/login-with-google-provider.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { LoginPage } from "./login";
+
+const googleClientId =
+  process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID ||
+  "194218724970-870jicr294njjlmg05e9is2n65sapolj.apps.googleusercontent.com";
+
+export const LoginWithGoogleProvider = () => {
+  return (
+    <GoogleOAuthProvider clientId={googleClientId}>
+      <LoginPage />
+    </GoogleOAuthProvider>
+  );
+};
